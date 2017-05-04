aa2c680ac372da7712c17cc64299b20d508c1cda
budzow <michal.budzowski@sonarsource.com>
2017-05-04 16:34:54 +0100

    Init project

---
+++ src/main/java/com/sonarsource/support/App.java
@@ -0,0 +1,21 @@
+package com.sonarsource.support;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        
+    }
+    
+    private void test(){
+        /*System.out.println( "Hello World!" );*/
+
+    }
+}
