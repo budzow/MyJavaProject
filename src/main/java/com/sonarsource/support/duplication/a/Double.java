56df3484bf7f7b10986d81ae8c2ffe92123b827f
budzow <michal.budzowski@sonarsource.com>
2017-05-29 12:28:31 +0200

    m:

--- src/main/java/com/sonarsource/support/duplication/a/Double.java
+++ src/main/java/com/sonarsource/support/duplication/a/Double.java
@@ -47,5 +47,17 @@ public class Double {
 		String.valueOf(55);
 	}
 
+	public void testX(){
+		String p = "22";
+		String r = "33";
+		String s = "44";
+		String t = "55";
+		String q = "66";
+		
+		(p+r+s+t+q).toString();
+		
+		String.valueOf(44);
+		String.valueOf(55);
+	}	
 	
 }
